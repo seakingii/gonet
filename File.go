9a1a76dccb41e4e560572f886b31e18ffbf1567a
seakingii <seaking2@qq.com>
2022-03-11 09:59:07 +0800

    File.WriteAllText

--- File.go
+++ File.go
@@ -1,2 +1,28 @@
 package gonet
 
+import (
+	"os"
+)
+
+func WriteAllText(path string, content:string) (err Error) {
+	
+	//os.O_WRONLY | os.O_CREATE
+	file, err := os.OpenFile(path, os.O_WRONLY |os.O_TRUNC | os.O_CREATE, 0666)
+	if err!=nil{		
+		return err
+	}
+	defer file.Close() 
+	 
+ 
+    //使用缓存方式写入
+    writer := bufio.NewWriter(file)
+ 
+    count, w_err := writer.WriteString(content)
+ 
+	//将缓存中数据刷新到文本中
+    writer.Flush()
+
+	return w_err
+
+
+}
